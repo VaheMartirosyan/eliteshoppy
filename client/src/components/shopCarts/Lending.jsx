56aed264ac878cf7a18627f763d1cfbd1dfeeb0c
VaheMartirosyan <vahemartirosyan017@icloud.com>
2019-11-08 13:06:49 +0400

    vol2.2.1

--- client/src/components/shopCarts/Lending.jsx
+++ client/src/components/shopCarts/Lending.jsx
@@ -1,77 +1,91 @@
-import React from 'react'
-import './shopcarts.css'
+import React from 'react';
+import './shopCarts.scss'
 import {setProduct} from '../UserFunctions'
 import Spiner from '../Spiner/Spiner'
 
 export default class Shopcart extends React.Component {
     state={
         products:[],
-        loading:true
+        loading:true,
+        active:null,
     }
     componentDidMount(){
         setProduct("http://localhost:5000/stok/getProductCarts","GET")
-        .then(body =>{
-            this.getProduct(body)
-        })
-        .catch(err => console.log(err))
-       
+            .then(body =>{
+                this.getProduct(body)
+            })
+            .catch(err => console.log(err))
+
     }
 
     getProduct = body => this.setState({products:body,loading:false});
-   
-   render(){
-       if(this.state.loading){
-           return <Spiner />
-       }
-       return (
-           <div className="shopitm_wrap">
-           <h3 className="wthree_text_info">New <span>Arrivals</span></h3>
-           <div className="cartNavigate">
-           <ul className="resp-tabs-list">
-							<li className="resp-tab-item resp-tab-active" aria-controls="tab_item-0" role="tab"> Men's</li>
-							<li className="resp-tab-item" aria-controls="tab_item-1" role="tab"> Women's</li>
-							<li className="resp-tab-item" aria-controls="tab_item-2" role="tab"> Bags</li>
-							<li className="resp-tab-item" aria-controls="tab_item-3" role="tab"> Footwear</li>
-						</ul>
-           </div>
-           <div className='shopItem'>
-      
-          {this.state.products.map((item,index)=>{
-              return  <div className="wrap" key={index}>
-               <div className="imgConteiner"><img src={`./img/${item.img}`} alt="shoose"/>
-              <div className="item-info-product">
-              <span className="product-new-top">New</span>
-               <h4 className="title">{item.goods_name}</h4>
-               <div className="info-product-price">
-                   <span>${item.price}</span>
-               </div>
-               <div className="snipcart-details top_brand_home_details item_add single-item hvr-outline-out button2">
-              <form >
-                        <fieldset>
-                        <input type="hidden" name="cmd" value={item._id}/>
-                            <input type="hidden" name="add" value="1"/>
-                            <input type="hidden" name="business" value=" "/>
-                            <input type="hidden" name="item_name" value="Formal Blue Shirt"/>
-                            <input type="hidden" name="amount" value="30.99"/>
-                            <input type="hidden" name="discount_amount" value="1.00"/>
-                            <input type="hidden" name="currency_code" value="USD"/>
-                            <input type="hidden" name="return" value=" "/>
-                            <input type="hidden" name="cancel_return" value=" "/>
-                            <input type="submit" name="submit" value="Add to cart" className="button"/>
-                        </fieldset>
-                    </form>
+
+    activeclass(e,i){
+        this.setState({
+            active:e
+        })
+    }
+    render(){
+
+        if(this.state.loading){
+            return <Spiner />
+        }
+
+        const ad = ["Men`s", "Women`s", "Kid`s"];
+
+        return (
+            <div className="shops">
+                <h2 className="wthree_text_info">New <span>Arrivals</span></h2>
+                <div className="cartNavigate">
+
+                    <ul className="resp-tabs-list">
+                        {ad.map((el, i) => {
+                            return(
+                                <li key={i} onClick={this.activeclass.bind(this,i)} className={ i == this.state.active ? 'actives' : 'notactive'}>{el}</li>
+                            )
+                        })}
+                    </ul>
+
                 </div>
-               </div>
-              
-              </div>
-             
-           </div>
-          })}
-        
-        </div>
-           </div>
-       
-      
-    )
-}
+                <div className={'shopCartscontainer'}>
+                    <div className={'carts'}>
+                        {this.state.products.map((item,index)=>{
+                            return (
+
+                                <div key={index} className={'shopCarts'}>
+                                    <div>
+                                        <img src={`./img/${item.img}`} alt="shoose"/>
+                                    </div>
+                                    <div className={'cartitemname'}>
+                                        <h4 className="title">{item.goods_name}</h4>
+                                    </div>
+                                    <div className={'cartprice'}>
+                                        <span>${item.price}</span>
+                                    </div>
+                                    <span className={'new'}>New</span>
+                                    <div className={'cartbutton'}>
+                                   <span className={'cartaddhover'}>
+                                       <form >
+                                       <fieldset>
+                                           <input type="hidden" name="cmd" value={item._id}/>
+                                           <input type="hidden" name="add" value="1"/>
+                                           <input type="hidden" name="return" value=" "/>
+                                           <input type="hidden" name="cancel_return" value=" "/>
+                                           <input type="submit" name="submit" value="Add to cart" className="button"/>
+                                       </fieldset>
+                                   </form>
+                                   </span>
+                                    </div>
+                                </div>
+
+                            )
+                        })}
+                    </div>
+                </div>
+            </div>
+
+
+
+        )
+    }
 }
