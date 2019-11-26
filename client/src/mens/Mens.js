8ed9215407ca4180192f232aa57bc30ac5cff8d4
Vahe <55959474+VaheMartirosyan@users.noreply.github.com>
2019-11-26 12:45:23 +0400

    added mens and womens

--- client/src/mens/Mens.js
+++ client/src/mens/Mens.js
@@ -1,28 +1,54 @@
 import React, {Component} from 'react'
 import Wear from "../wear/Wear";
+import './Mens.scss'
 import {setProduct} from '../components/UserFunctions'
 import FilterWear from "../wear/filterwear/FilterWear";
+import WearCards from "../wear/filterwear/wearcards/WearCards";
+import Spiner from '../components/Spiner/Spiner'
 
 export default class Mens extends Component {
     state = {
         wearTitle: 'MEN\'S WEAR',
-        products:''
+        products: [],
+        loading: true
     }
     componentDidMount(){
         setProduct(`stok/menproduct`,"GET")
             .then(body =>{
                 this.setState({products:body})
-               
+                this.getProduct(body)
+
             })
             .catch(err => console.log(err))
     }
+    getProduct = body => this.setState({products:body, loading:false})
+
     render() {
+        if(this.state.loading) {
+            return <Spiner />
+        }
         console.log(this.state.products);
         const {wearTitle} = this.state
         return(
             <div>
                 <Wear wear={wearTitle} />
-                <FilterWear />
+                <FilterWear imgone={'https://demo.w3layouts.com/demos_new/template_demo/20-06-2017/elite_shoppy-demo_Free/143933984/web/images/banner2.jpg'}
+                            imgtwo={'https://demo.w3layouts.com/demos_new/template_demo/20-06-2017/elite_shoppy-demo_Free/143933984/web/images/banner5.jpg'}
+                            imgthree={'https://demo.w3layouts.com/demos_new/template_demo/20-06-2017/elite_shoppy-demo_Free/143933984/web/images/banner2.jpg'}
+                            imgfour={'https://demo.w3layouts.com/demos_new/template_demo/20-06-2017/elite_shoppy-demo_Free/143933984/web/images/bb2.jpg'}
+                />
+                <div className="container">
+                    <div className={'cardwears'}>
+                        {this.state.products.map((elem, index) => {
+
+                            return(
+                                <div key={index}>
+                                    <WearCards itemimg={elem.img} item={elem.goods_name} price={elem.price}/>
+                                </div>
+                            )
+                        }) }
+                    </div>
+                </div>
             </div>
         )
     }
--- client/src/womens/Womens.js
+++ client/src/womens/Womens.js
@@ -1,19 +1,61 @@
-import React, { useState, useEffect } from "react"
+import React, { Component } from "react"
 import {setProduct} from '../components/UserFunctions'
-export default () => {
-    let [womenProducts, setStates] = useState('');
-    useEffect(()=>{
+import Wear from "../wear/Wear";
+import FilterWear from "../wear/filterwear/FilterWear";
+import WearCards from "../wear/filterwear/wearcards/WearCards"
+import Spiner from "../components/Spiner/Spiner";
+
+// let [womenProducts, setStates] = useState('');
+// useEffect(()=>{
+//     setProduct(`stok/womenproduct`,"GET")
+//     .then(prod=>{
+//         setStates(womenProducts = prod)
+//   })
+// },[])
+// console.log(womenProducts + 'womens')
+
+export default class Womens extends Component {
+
+    state = {
+        wearTitle: 'MEN\'S WEAR',
+        products: [],
+        loading: true
+    }
+    componentDidMount(){
         setProduct(`stok/womenproduct`,"GET")
-        .then(prod=>{
-            setStates(womenProducts = prod)
-        
-      })
-    },[])
-    console.log(womenProducts)
-    return(
-        
-        <div>
-            <h1>Womens</h1>
-        </div>
-    )
+            .then(body =>{
+                this.setState({products:body})
+                this.getProduct(body)
+
+            })
+            .catch(err => console.log(err))
+    }
+    getProduct = body => this.setState({products:body, loading:false})
+    render() {
+        if(this.state.loading) {
+            return  <Spiner />
+        }
+        return(
+            <div>
+                <Wear wear={'Womens Wear'} />
+                <FilterWear imgone={'https://demo.w3layouts.com/demos_new/template_demo/20-06-2017/elite_shoppy-demo_Free/143933984/web/images/banner4.jpg'}
+                            imgtwo={'https://demo.w3layouts.com/demos_new/template_demo/20-06-2017/elite_shoppy-demo_Free/143933984/web/images/banner3.jpg'}
+                            imgthree={'https://demo.w3layouts.com/demos_new/template_demo/20-06-2017/elite_shoppy-demo_Free/143933984/web/images/banner1.jpg'}
+                            imgfour={'https://demo.w3layouts.com/demos_new/template_demo/20-06-2017/elite_shoppy-demo_Free/143933984/web/images/bb1.jpg'}
+                />
+                <div className="container">
+                    <div className={'cardwears'}>
+                        {this.state.products.map((elem, index) => {
+
+                            return(
+                                <div key={index}>
+                                    <WearCards itemimg={elem.img} item={elem.goods_name} price={elem.price}/>
+                                </div>
+                            )
+                        }) }
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
